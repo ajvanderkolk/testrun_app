5fcc5549ae4b6f37d0be47d0b1fc89d43092e498
Andrew <ajvanderkolk@wisc.edu>
2023-04-23 18:16:34 -0500

    ensured python works by itself

--- lib/extract_PDF.py
+++ lib/extract_PDF.py
@@ -1,36 +1,27 @@
-import PyPDF2
+from tkinter import filedialog
+from tkinter import *
+from PyPDF2 import PdfReader
 import nltk
 from nltk.corpus import stopwords
-import sys
-import logging
-import subprocess
 
-# configure logging to print to console
-logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(levelname)s %(message)s')
+# create a Tkinter root window
+root = Tk()
+root.withdraw()  # hide the root window
 
-# get the path of the selected file
-file_path = sys.argv[1]
-
-processResult = subprocess.run(['python', 'extract_text.py', file_path], capture_output=True, text=True)
-
-# use logging to print to console
-logging.info('Python Console Output: {}'.format(processResult.stdout))
+# prompt user to select a file
+file_path = filedialog.askopenfilename(filetypes=[('PDF files', '*.pdf')])
 
 # create pdf reader object
-with open(file_path, 'rb') as file:
-    reader = PyPDF2.PdfFileReader(file)
+reader = PdfReader(file_path)
 
-# printing number of pages in pdf fileno
+# printing number of pages in pdf file
 print(len(reader.pages))
 
 # iterate over all pages in pdf file
-for page_num in range(reader.numPages):
-    # get the page object
-    page = reader.getPage(page_num)
-
+for page_num, page in enumerate(reader.pages):
     # extracting text from page
-    text = page.extractText()
-    print(f"Extracted text from page {page_num + 1}: {text}")
+    text = page.extract_text()
+    print(text)
 
     # tokenize and clean text
     tokens = nltk.word_tokenize(text)
@@ -39,6 +30,6 @@ for page_num in range(reader.numPages):
     clean_text = [word.lower() for word in filtered_text if word.isalpha()]
 
     # write clean text to file
-    with open('extracted_text.txt', 'a') as f:
+    with open('extracted_text_three.txt', 'a') as f:
         f.write(f'Page {page_num+1}:\n')
         f.write(' '.join(clean_text) + '\n')
